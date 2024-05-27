fa9be9b254df9108493c573f1d81b57c6ec03b65
Juan <juanburillodev@gmail.com>
2024-05-27 17:32:28 +0200

    Change nav link font weight

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-primary text-sm font-bold leading-5 focus:outline-none focus:border-gray-400 transition duration-150 ease-in-out'
+        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-primary text-sm font-medium leading-5 focus:outline-none focus:border-gray-400 transition duration-150 ease-in-out'
         : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 hover:text-gray-700 hover:border-gray-400 focus:outline-none focus:text-gray-700 focus:border-gray-400 transition duration-150 ease-in-out'
 );
 </script>
