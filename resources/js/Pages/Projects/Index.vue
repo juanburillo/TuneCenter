dee5485c135176126091f73a177d4222543e25c4
Juan <juanburillodev@gmail.com>
2024-05-30 16:16:22 +0200

    Create delete project confirmation modal

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -6,13 +6,19 @@ import InputError from '@/Components/InputError.vue';
 import TextInput from '@/Components/TextInput.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import Modal from '@/Components/Modal.vue';
 
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(relativeTime);
+
+const confirmingProjectDeletion = ref(false);
+const projectToDelete = ref(null);
 
 defineProps({
     projects: {
@@ -33,13 +39,29 @@ const submitCreateForm = () => {
     });
 };
 
-const handleProjectDelete = (projectId) => {
-    deleteForm.delete(route('projects.destroy', projectId));
+const handleProjectDelete = () => {
+    if (projectToDelete.value) {
+        deleteForm.delete(route('projects.destroy', projectToDelete.value), {
+            onSuccess: () => {
+                closeModal();
+            },
+        });
+    }
 };
 
 const formatUpdatedAt = (date) => {
     return dayjs(date).fromNow(true);
 };
+
+const confirmProjectDeletion = (projectId) => {
+    projectToDelete.value = projectId;
+    confirmingProjectDeletion.value = true;
+}
+
+const closeModal = () => {
+    confirmingProjectDeletion.value = false;
+    projectToDelete.value = null;
+}
 
 </script>
 
@@ -83,12 +105,14 @@ const formatUpdatedAt = (date) => {
                         <p class="mt-4">You don't have any projects yet... Create one and start making music!</p>
                     </div>
                     <div class="p-6 flex space-x-2" v-else v-for="project in projects" :key="project.id">
+                        <!-- Collaborative project icon -->
                         <svg v-if="project.is_collaborative" xmlns="http://www.w3.org/2000/svg" fill="none"
                             viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="w-6 h-6 text-gray-600 -scale-x-100">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                         </svg>
+                        <!-- Individual project icon -->
                         <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-600 -scale-x-100">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -109,7 +133,7 @@ const formatUpdatedAt = (date) => {
                             <SecondaryButton class="mt-4">Open</SecondaryButton>
                             </Link>
                         </div>
-                        <svg @click="handleProjectDelete(project.id)" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        <svg @click="confirmProjectDeletion(project.id)" xmlns="http://www.w3.org/2000/svg" fill="none"
                             viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="h-5 w-5 text-gray-600 cursor-pointer hover:text-gray-800 transition ease-in-out duration-150">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -119,5 +143,25 @@ const formatUpdatedAt = (date) => {
                 </div>
             </div>
         </div>
+
+        <Modal :show="confirmingProjectDeletion" @close="closeModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900">
+                    Are you sure you want to delete this project?
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600">
+                    This action is irreversible. Deleting this project will permanently remove all
+                    associated elements.
+                </p>
+
+                <div class="mt-6 flex">
+                    <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+
+                    <DangerButton @click="handleProjectDelete" class="ms-3">Delete Project
+                    </DangerButton>
+                </div>
+            </div>
+        </Modal>
     </AuthenticatedLayout>
 </template>
