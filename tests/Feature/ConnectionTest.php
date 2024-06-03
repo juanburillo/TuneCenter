134af2f2b60c4d53b620780e8d1f1b4da92e2c04
Juan <juanburillodev@gmail.com>
2024-06-03 17:14:55 +0200

    test: :white_check_mark: Add store and index connection tests

--- tests/Feature/ConnectionTest.php
+++ tests/Feature/ConnectionTest.php
@@ -2,19 +2,51 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ConnectionTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    private $senderUser;
+    private $recipientUser;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->senderUser = User::factory()->create();
+        $this->recipientUser = User::factory()->create();
+    }
+
+    public function test_connections_screen_can_be_rendered(): void
+    {
+        // When
+        $response = $this->actingAs($this->senderUser)->get('/connections');
+
+        // Then
         $response->assertStatus(200);
+    }
+
+    public function test_connection_request_can_be_sent(): void
+    {
+        // Given
+        $recipientUserData = [
+            'username' => $this->recipientUser->username,
+        ];
+
+        // When
+        $response = $this->actingAs($this->senderUser)->post('/connections', $recipientUserData);
+
+        // Then
+        $response->assertRedirect(route('connections.index'));
+        $this->assertDatabaseHas('connections', [
+            'sender_id' => $this->senderUser->id,
+            'recipient_id' => $this->recipientUser->id,
+            'accepted' => false,
+        ]);
     }
 }
