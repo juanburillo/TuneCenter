faf20dd2648cb9d0e029418c3316d4c0b5f8c398
Juan <juanburillodev@gmail.com>
2024-06-04 14:48:05 +0200

    style: :lipstick: Fix autofocus on wrong input

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -51,7 +51,6 @@ const submit = () => {
                     class="mt-1 block w-full"
                     v-model="form.username"
                     required
-                    autofocus
                     autocomplete="username"
                 />
 
