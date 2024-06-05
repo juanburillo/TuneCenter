a7d1055c46f6076008d6a50f92217a9482d4e6af
Juan <juanburillodev@gmail.com>
2024-06-05 10:01:01 +0200

    feat: :sparkles: Create TextAreaInput component

---
+++ resources/js/Components/TextAreaInput.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const model = defineModel({
+    type: String,
+    required: true,
+});
+
+const textarea = ref(null);
+</script>
+
+<template>
+    <textarea class="border-gray-300 focus:border-primary focus:ring-primary rounded-md shadow-sm" v-model="model" ref="textarea">
+        <slot />
+    </textarea>
+</template>
